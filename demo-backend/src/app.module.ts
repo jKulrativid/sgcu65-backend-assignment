304ab3aa6ad4d909e5505d9bc8adef8da2ece870
K. Jetnipit <jetnipit54@gmail.com>
2022-08-05 03:20:39 +0700

    Add user model to Nest.js module

--- demo-backend/src/app.module.ts
+++ demo-backend/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './controller/app.controller';
 import { UserController } from './controller/user.controller';
+
 import { AppService } from './service/app.service';
 import { UserService } from './service/user.service';
 
+import { User } from './model/user.model';
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule { }
