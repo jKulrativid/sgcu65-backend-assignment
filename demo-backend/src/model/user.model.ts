988e8fd1cbbbd92d3c6fcb9c5f05167976e9a168
K. Jetnipit <jetnipit54@gmail.com>
2022-08-05 03:17:29 +0700

    Implement user model
    Install Nest.js class validator and typeorm

--- demo-backend/src/model/user.model.ts
+++ demo-backend/src/model/user.model.ts
@@ -1,7 +1,22 @@
-export interface User {
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail } from 'class-validator';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
   id: string;
+
+  @Column({ length: 255 })
+  @IsEmail()
   email: string;
+
+  @Column({ length: 255 })
   firstName: string;
+
+  @Column({ length: 255 })
   surName: string;
+
+  @Column({ length: 255 })
   role: string;
+
 }
